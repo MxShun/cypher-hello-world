66d8da3369dd9dda38871d476822ba4622a63329
MxShun <16466322+MxShun@users.noreply.github.com>
2021-10-01 19:51:06 +0900

    detekt-formattingを導入する

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.18.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
