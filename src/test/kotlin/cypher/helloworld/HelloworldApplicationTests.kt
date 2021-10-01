02b9adee30687e9b7e83dcdcc1192bc3367d72a5
MxShun <16466322+MxShun@users.noreply.github.com>
2021-10-01 09:04:08 +0900

    初めてのSpring Initializr

---
+++ src/test/kotlin/cypher/helloworld/HelloworldApplicationTests.kt
@@ -0,0 +1,13 @@
+package cypher.helloworld
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
