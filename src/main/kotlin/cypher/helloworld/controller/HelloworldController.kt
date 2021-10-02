c34bf6b92f8012b0706465ed4e2204ee20c084a7
MxShun <16466322+MxShun@users.noreply.github.com>
2021-10-02 19:24:46 +0900

    model->entity

--- src/main/kotlin/cypher/helloworld/controller/HelloworldController.kt
+++ src/main/kotlin/cypher/helloworld/controller/HelloworldController.kt
@@ -1,6 +1,6 @@
 package cypher.helloworld.controller
 
-import cypher.helloworld.model.Greet
+import cypher.helloworld.entity.Greet
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
--- src/main/kotlin/cypher/helloworld/entity/Greet.kt
+++ src/main/kotlin/cypher/helloworld/entity/Greet.kt
@@ -1,4 +1,4 @@
-package cypher.helloworld.model
+package cypher.helloworld.entity
 
 class Greet(
     val message: String,
