5666b84e71825bb77d0c4cedac831a681be8976b
MxShun <16466322+MxShun@users.noreply.github.com>
2021-10-03 20:33:54 +0900

    不要なインポートを削除

--- src/main/kotlin/cypher/helloworld/advice/ExceptionHandlerAdvice.kt
+++ src/main/kotlin/cypher/helloworld/advice/ExceptionHandlerAdvice.kt
@@ -3,7 +3,6 @@ package cypher.helloworld.advice
 import cypher.helloworld.entity.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.NoHandlerFoundException
