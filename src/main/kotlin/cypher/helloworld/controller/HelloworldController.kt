3c59c1f61b7c5838e85e58ccd66a37b91475806f
MxShun <16466322+MxShun@users.noreply.github.com>
2021-10-04 23:49:34 +0900

    nameのバリデート実装

--- src/main/kotlin/cypher/helloworld/controller/HelloworldController.kt
+++ src/main/kotlin/cypher/helloworld/controller/HelloworldController.kt
@@ -1,14 +1,22 @@
 package cypher.helloworld.controller
 
 import cypher.helloworld.entity.Reply
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.constraints.Size
 
+@Validated
 @RestController
 class HelloworldController {
     @GetMapping("/")
     fun helloworld(
-        @RequestParam name: String,
+        @RequestParam @Size(min = NAME_MINIMUM_LENGTH, max = NAME_MAXIMUM_LENGTH) name: String,
     ): Reply = Reply(message = "Hello, $name")
+
+    companion object {
+        private const val NAME_MINIMUM_LENGTH = 3
+        private const val NAME_MAXIMUM_LENGTH = 10
+    }
 }
