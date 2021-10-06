833b18286a60fc4d591e4eb4f96377fda450b72f
MxShun <16466322+MxShun@users.noreply.github.com>
2021-10-06 22:53:25 +0900

    SpringProfilesごとにレスポンスtextを出し分け

--- src/main/kotlin/cypher/helloworld/controller/HelloworldController.kt
+++ src/main/kotlin/cypher/helloworld/controller/HelloworldController.kt
@@ -1,6 +1,7 @@
 package cypher.helloworld.controller
 
 import cypher.helloworld.entity.Reply
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -12,8 +13,9 @@ import javax.validation.constraints.Size
 class HelloworldController {
     @GetMapping("/")
     fun helloworld(
+        @Value(value = "\${helloworld.text}") text: String,
         @RequestParam @Size(min = NAME_MINIMUM_LENGTH, max = NAME_MAXIMUM_LENGTH) name: String,
-    ): Reply = Reply(message = "Hello, $name")
+    ): Reply = Reply(message = "Hello, $text, $name")
 
     companion object {
         private const val NAME_MINIMUM_LENGTH = 3
