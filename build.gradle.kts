92794baf79f6ec1fd24dfb8bc709ee8d91a0e0d7
MxShun <16466322+MxShun@users.noreply.github.com>
2021-10-10 21:14:46 +0900

    ヘルスチェックを有効化

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ detekt {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
